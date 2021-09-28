f08754ccfc6e22297e5bc734e2b283ff0e527370
abdtyx <1507844577@qq.com>
2021-09-28 23:25:44 +0800

    updated

--- tyx/Python/QQbot/bot_Azure/bot_Azure.py
+++ tyx/Python/QQbot/bot_Azure/bot_Azure.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
 import urllib.request
-
+import urllib.parse
 # msg = "我是一个bot"
-
+# 卡中文解法：urllib.parse.quote("中文")
+msg = urllib.parse.quote("Azure在这一天学会了中文")
+url = "http://127.0.0.1:5700/send_group_msg?group_id=913813594&message=" + msg
 try:
-    html = urllib.request.urlopen("http://127.0.0.1:5700/send_group_msg?group_id=913813594&message=I'm%20not%20a%20bot", timeout=5)
+    html = urllib.request.urlopen(url, timeout=5)
     print(html.read())
 except Exception as result:
     print(result)
