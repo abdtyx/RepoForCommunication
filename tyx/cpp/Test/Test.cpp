cbaeefcb658f0d4b7404835cf2e99563e38671e9
abdtyx <1507844577@qq.com>
2021-10-08 16:19:11 +0800

    updated

--- tyx/cpp/Test/Test.cpp
+++ tyx/cpp/Test/Test.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
-int main(){
-    char a, b, c, d, e, f, g, h, i, j, k, l, m, n;
-    a = 0x53 ; b = 0x4d ;c =0x41 ; d = 0x52 ; e = 0x54; f = 0x2b; g = 0x42; h = 0x45; i = 0x41 ; j = 0x55 ;k = 0x54; l = 0x59; m =0x3d;n = 0x55;
-    cout << a << b << c << d<< e<< f<< g<< h<<i<< j<< k<< l<< m<< n;
+
+int main() {
+    system("title Cprogram");
+    system("mode con cols=48 lines=25");
+    system("color 0B");
+    system("date /T");
+    system("TIME /T");
+    system("pause");
 }
