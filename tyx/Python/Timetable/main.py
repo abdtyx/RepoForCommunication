7ef5c3ce68e1da3397a1df41722b4b2485d6c92f
abdtyx <1507844577@qq.com>
2021-10-11 23:52:30 +0800

    Framework done.

---
+++ tyx/Python/Timetable/main.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from DayTableInfoAPI.DayTableInfoAPI import DayTableInfo
+
+
+
+def main():
+    text = DayTableInfo()
+    print(text)
+    '''
+    对接Bot_Azure
+    '''
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
