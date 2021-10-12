0744b9118a329f034ae071c57d08b6ce929c0634
abdtyx <1507844577@qq.com>
2021-10-12 19:22:16 +0800

    DayTableInfoAPI done.

--- tyx/Python/Timetable/GetChangeInfo/GetChangeInfo.py
+++ tyx/Python/Timetable/GetChangeInfo/GetChangeInfo.py
@@ -1,1 +1,5 @@
 # coding=utf-8
+
+# fetch调课url，调用handle处理得到调课课表列表，直接调整Table.txt，不返回任何值
+def Change():
+    print()
--- tyx/Python/Timetable/GetTable/GetTable.py
+++ tyx/Python/Timetable/GetTable/GetTable.py
@@ -1,1 +1,6 @@
 # coding=utf-8
+
+# 去访问课表url，拿到html源码，交给handle模块处理，最后存储为Table.txt，不返回任何值
+
+def Table():
+    print()
