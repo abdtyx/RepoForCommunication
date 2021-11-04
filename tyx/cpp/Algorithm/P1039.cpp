1cf54c7793abbbd40cec96551ead625ce9b4687a
abdtyx <1507844577@qq.com>
2021-11-04 19:26:57 +0800

    updated

--- tyx/cpp/Algorithm/P1039.cpp
+++ tyx/cpp/Algorithm/P1039.cpp
@@ -11,20 +11,25 @@ int main() {
     pair<string, bool> person[25];  // pair<name, guilty>
     // int person_length = 0;
     pair<string, string> sentence[105];  // pair<name, evidence>
+    vector<string> ans;
+    ans.clear();
     regex r("(.*): (.*)");
     smatch _m;
-    for (int i = 0; i < p; i++) {
-        string reg;
-        cin >> reg;
-        regex_search(reg, _m, r);
-        sentence[i].first = _m.str(1);
-        sentence[i].second = _m.str(2);
-    }
     for (int i = 0; i < m; i++) {
         string tmp;
         cin >> tmp;
         person[i + 1].first = tmp;
         person[i + 1].second = false;
+    }
+    char abandon;
+    cin >> abandon;
+    for (int i = 0; i < p; i++) {
+        string reg;
+        // cin >> reg;
+        getline(cin, reg);
+        regex_search(reg, _m, r);
+        sentence[i].first = _m.str(1);
+        sentence[i].second = _m.str(2);
     }
     for (int tot = 0, mask = (1 << m); tot < mask; tot++) {
         int select = 0; // select用来记录取了几个人，只对取了n个人的情况进行计算
@@ -35,13 +40,13 @@ int main() {
         pair<int, int> is_guilty[25];   // pari<guilty, not_guilty> 
         // initialize
         for (int z = 0; z < 25; z++) {
-            truth[z] = false;
+            truth[z] = true;
             is_guilty[z].first = 0;
             is_guilty[z].second = 0;
             person[z].second = false;
         }
         for (int i = 0; i < m; i++) {
-            if ((tot << i) & 1) {
+            if ((1 << i) & tot) {
                 select++;
                 truth[i + 1] = false;
             }
@@ -84,7 +89,7 @@ int main() {
                     }
                 }
                 // I am not guilty.
-                if (sentence[j].second == "I am not guilty.") {
+                else if (sentence[j].second == "I am not guilty.") {
                     // find person number
                     string tmp = sentence[j].first;
                     int person_number = 1;
@@ -109,13 +114,92 @@ int main() {
                             is_guilty[person_number].first++;
                     }
                 }
-                // XXX is guilty
                 // find person number
-                for (int person_number = 1; person_number < 25; person_number++) {
-                    if (person[person_number].first + " is guilty." == )
+                else {
+                    bool go_to_1 = false, go_to_2 = false;
+                    int person_number = 1;
+                    for (; person_number < 25; person_number++) {
+                        if (person[person_number].first + " is guilty." == sentence[j].second) {
+                            go_to_1 = true;
+                            break;
+                        }
+                        else if (person[person_number].first + " is not guilty." == sentence[j].second) {
+                            go_to_2 = true;
+                            break;
+                        }
+                    }
+                    if (go_to_1) {
+                        // XXX is guilty
+                        // find sentence_number
+                        string tmp = sentence[j].first;
+                        int sentence_number = 1;
+                        for (; sentence_number < 25; sentence_number++) {
+                            if (person[sentence_number].first == tmp)
+                                break;
+                        }
+                        if (truth[sentence_number]) {
+                            if (is_guilty[person_number].second != 0) {
+                                flag = false;
+                                break;
+                            }
+                            else
+                                is_guilty[person_number].first++;
+                        }
+                        else {
+                            if (is_guilty[person_number].first != 0) {
+                                flag = false;
+                                break;
+                            }
+                            else
+                                is_guilty[person_number].second++;
+                        }
+                    }
+                    else if (go_to_2) {
+                        // XXX is not guilty
+                        // find sentence_number
+                        string tmp = sentence[j].first;
+                        int sentence_number = 1;
+                        for (; sentence_number < 25; sentence_number++) {
+                            if (person[sentence_number].first == tmp)
+                                break;
+                        }
+                        if (truth[sentence_number]) {
+                            if (is_guilty[person_number].first != 0) {
+                                flag = false;
+                                break;
+                            }
+                            else
+                                is_guilty[person_number].second++;
+                        }
+                        else {
+                            if (is_guilty[person_number].second != 0) {
+                                flag = false;
+                                break;
+                            }
+                            else 
+                                is_guilty[person_number].first++;
+                        }
+                    }
                 }
-                // XXX is not guilty.
+            }
+            // 语句判定完了，现在看该解是否成立
+            if (flag) {
+                // 不起冲突
+                vector<string> possible;
+                possible.clear();
+                for (int k = 1; k <= m; k++) {
+                    if (is_guilty[k].first)
+                        possible.push_back(person[k].first);
+                }
+                if (possible.size() == 1)
+                    ans.push_back(possible[0]);
             }
         }
     }
+    if (ans.size() > 1)
+        cout << "Cannot Determine";
+    else if (ans.size() == 0)
+        cout << "Impossible";
+    else
+        cout << ans[0];
 }
