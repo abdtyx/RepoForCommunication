d9a30a22c7084b9240f61a5cbf5b74a712fab0bf
abdtyx <1507844577@qq.com>
2021-12-01 21:11:38 +0800

    Add a program that can find MTU

---
+++ tyx/cpp/Ping_202.117.0.20/FindMTU.cpp
@@ -0,0 +1,64 @@
+// to_where的值就是目标地址
+// 该程序可以找到从本地到目标地址的MTU
+// 核心算法：二分查找
+// 默认找202.117.0.20
+
+// Error code
+// 65500: 表示本机具有将已拆分的数据包合成并验证回复的能力或者MTU大于65500，后者可能性极小。
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    FILE* fp = NULL;
+    char* cmd = new char[512];
+    string pin = "ping -l ";
+    string to_where = " 202.117.0.20";
+    int left = 0, right = 65500;
+    int ans = (left + right) / 2;
+    string find_ans = pin + to_string(ans) + to_where;
+    sprintf(cmd, find_ans.c_str());
+    while (1) {
+        if ((fp = popen(cmd, "r")) != NULL) {
+            while (1) {
+                if (fgets(cmd, 100, fp) != NULL) {
+                    if (string(cmd) == "Request timed out.\n") {
+                        // to lower
+                        right = ans;
+                        ans = (left + right) / 2;
+                        if (ans == right - 1) {
+                            printf("The MTU from here to target ip is: %d", ans);
+                            return 0;
+                        }
+                        printf("Next: %d\n", ans);
+                        find_ans = pin + to_string(ans) + to_where;
+                        sprintf(cmd, find_ans.c_str());
+                        break;
+                    }
+                    if (string(cmd) == "Ping statistics for" + to_where + ":\n") {
+                        // to upper
+                        left = ans;
+                        ans = (left + right + 1) / 2;
+                        if (ans == right) {
+                            if (ans == 65500) {
+                                printf("Cannot find the MTU from here to target ip. \nError: 65500.");
+                            }
+                            printf("The MTU from here to target ip is: %d", ans);
+                            return 0;
+                        }
+                        printf("Next: %d\n", ans);
+                        find_ans = pin + to_string(ans) + to_where;
+                        sprintf(cmd, find_ans.c_str());
+                        break;
+                    }
+                }
+                else
+                    break;
+            }
+        }
+        else 
+            break;
+    }
+    return 0;
+}
---
+++ tyx/cpp/Ping_202.117.0.20/test.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define N 100
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+
+using namespace std;
+
+int main() {
+    FILE* fp = NULL;
+    char* cmd = new char[512];
+    // system("echo hello");
+    sprintf(cmd, "ping -l 65500 202.117.0.20");
+    if ((fp = popen(cmd, "r")) != NULL) {
+        while (1) {
+            if (fgets(cmd, 100, fp) != NULL) {
+                printf("%s", cmd);
+            }
+            // if (fscanf(fp, "%[^\n]", cmd) != -1)
+            //     printf("%s", cmd);
+            else
+                break;
+            // pclose(fp);
+        }
+    }
+    // printf("%s", cmd);
+    return 0;
+}
