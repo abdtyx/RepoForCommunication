c77dfa1eeab02aecc8e612363d60e5045c2a87cf
abdtyx <1507844577@qq.com>
2021-12-24 22:36:34 +0800

    updated

--- tyx/cpp/cpphomework/66.cpp
+++ tyx/cpp/cpphomework/66.cpp
@@ -17,7 +17,47 @@ public:
     ~Cd();
     virtual void Report() const;
     Cd &operator=(const Cd &d);
+    const char* performers();
+    const char* label();
+    const int selections();
+    const double playtime();
+    void c_performers(const char* s);
+    void c_label(const char* s);
+    void c_selections(const int i);
+    void c_playtime(const double d);
 };
+
+void Cd::c_performers(const char* s) {
+	strcpy(performers, s);
+}
+
+void Cd::c_label(const char* s) {
+	strcpy(label, s);
+}
+
+void Cd::c_selections(const int i) {
+	selections = i;
+}
+
+void Cd::c_playtime(const double d) {
+	playtime = d;
+}
+
+const char* Cd::performers() {
+	return performers;
+}
+
+const char* Cd::label() {
+	return label;
+}
+
+const int Cd::selections() {
+	return selections;
+}
+
+const double playtime() {
+	return playtime;
+}
 
 Cd::Cd(const char* s1, const char *s2, int n, double x) {
 	strcpy(performers, s1);
@@ -47,7 +87,7 @@ Cd::~Cd() {
         playtime = 0;
 }
 
-Cd& operator=(const Cd& d) {
+Cd& Cd::operator=(const Cd& d) {
         strcpy(performers, d.performers);
         strcpy(label, d.label);
         selections = d.selections;
@@ -59,7 +99,7 @@ void Cd::Report() const {
 	cout << performers << ',' << label << ',' << selections << ',' << playtime << endl;
 }
 
-class Classic: public Cd {
+class Classic : public Cd {
 	private:
 		char primary_work[50];
 	public:
@@ -73,46 +113,54 @@ class Classic: public Cd {
 
 Classic::Classic(const char* s1, const char* s2, const char* s3, int n, double x){
 	strcpy(primary_work, s1);
-	strcpy(performers, s2);
-        strcpy(label, s3);
-        selections = n;
-        playtime = x;
+	// strcpy(performers, s2);
+	c_performers(s2);
+        // strcpy(label, s3);
+	c_label(s3);
+        // selections = n;
+	c_selections(n);
+        // playtime = x;
+	c_playtime(x);
 }
 
 Classic::Classic(const Classic& c) {
-	strcpy(primary_work, c.primary_work);
-        strcpy(performers, c.performers);
-	strcpy(label, c.label);
-        selections = c.selections;
-        playtime = c.playtime;
+        strcpy(primary_work, c.primary_work());
+	c_performers(c.performers());
+	c_label(c.label());
+	c_selections(c.selections());
+	c_playtime(c.playtime());
 }
 
 Classic::Classic() {
 	primary_work[0] = 0;
-	performers[0] = 0;
-	label[0] = 0;
-	selections = 0;
-	playtime = 0;
+//	performers[0] = 0;
+//	label[0] = 0;
+//	selections = 0;
+//	playtime = 0;
+	c_performers("\0");
+	c_label("\0");
+	c_selections(0);
+	c_playtime(0);
 }
 
 Classic::~Classic() {
         primary_work[0] = 0;
-        performers[0] = 0;
-        label[0] = 0;
-        selections = 0;
-        playtime = 0;
+	c_performers("\0");
+        c_label("\0");
+        c_selections(0);
+        c_playtime(0);
 }
 
-Classic& operator=(const Classic& c) {
-        strcpy(primary_work, c.primary_work);
-        strcpy(performers, c.performers);
-        strcpy(label, c.label);
-        selections = c.selections;
-        playtime = c.playtime;
+Classic& Classic::operator=(const Classic& c) {
+        strcpy(primary_work, c.primary_work());
+        c_performers(c.performers());
+        c_label(c.label());
+        c_selections(c.selections());
+        c_playtime(c.playtime());
 	return *this;
 }
 
-void Classic:Report() const {
+void Classic::Report() const {
 	cout << primary_work << ',' << performers << ',' << label << ',' << selections << ',' << playtime << endl;
 }
 
