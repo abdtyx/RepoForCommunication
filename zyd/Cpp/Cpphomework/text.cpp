ad78dd202fd7d5e14bf1d225d750efa3c06bca3a
lodineD <599125397@qq.com>
2022-01-24 11:07:07 +0800

    updated

--- zyd/Cpp/Cpphomework/text.cpp
+++ zyd/Cpp/Cpphomework/text.cpp
@@ -1,14 +1,28 @@
-
-#include <iostream>
-#include <string>
-#include <valarray>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    valarray<int> a1{1, 2, 3};
-    valarray<int> a2{4, 5, 6};
-    pair<valarray<int>, valarray<int>> p;
-    p = make_pair(a1, a2);
-    cout << p.second[2] << endl;
+    int a[20];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << endl;
+    }
+    int m, num = n;
+    cin >> m;
+    while (num)
+    {
+        if (m == a[num])
+        {
+            break;
+        }
+        num--;
+    }
     return 0;
 }
